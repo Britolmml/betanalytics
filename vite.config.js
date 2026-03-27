6102a1405b1b5130e999c9f0ff65cb34350f70ca
Britolmml <72675429+Britolmml@users.noreply.github.com>
2026-03-27 01:50:31 -0500

    force rebuild

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   // En desarrollo, redirige /api/football → localhost:3000/api/football
-  // (Vercel dev lo maneja automáticamente)
+  // (Vercel dev lo maneja automáticamente) 
 });
+"" 
